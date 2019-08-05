bd1be807d863b423ef547119c95ec7c2d188d842
Naresh Bharasagar <bharasagarn@gmail.com>
2019-08-05 16:48:14 +0530

    Modified Lab2 Problem1

--- Lab_2/Lab2_Problem1.cpp
+++ Lab_2/Lab2_Problem1.cpp
@@ -4,80 +4,164 @@
 #include <cmath>
 #include <vector>
 using namespace std;
+typedef long long int loo;
 
 struct node_ {
-	int X;
-	int Y;
+	loo X;
+	loo Y;
 	struct node_* next;
 };
 typedef struct node_ node;
+node* head = NULL;
 
-void AddFirst(node* head, int x, int y) {
-		node* newNode = new node;
+void AddFirst(loo x, loo y) {
+		node* newNode = (node*)malloc(sizeof(node));
 		newNode->X = x;
 		newNode->Y = y;
-		newNode-> next = head;
+		newNode->next = head;
 		head = newNode;
 }
 
-void DelFirst(node* head) {
-	node* temp = head;
-	head = head->next;
-	temp->next=NULL;
-	delete temp;
+loo DelFirst() {
+	if(head==NULL) return -1;
+	else {
+		node* temp = head;
+		head = head->next;
+		temp->next=NULL;
+		delete temp;
+		return 0;
+	}
 }
 
-void Del(node* head, int x, int y) {
-	node* temp = head;
-	if(temp->X==x && temp->Y==y) {
-		DelFirst(head);
-	}
+loo Del(loo x, loo y) {
+	if(head==NULL) return -1;
 	else {
-		node * prev = head;
-		temp = head->next;
-		while(true) {
-			if(temp->X==x && temp->Y==y) {
-				prev->next=temp->next;
-				temp->next=NULL;
-				delete temp;
-			}
-			else {
-				prev = temp;
-				temp = temp->next;
+		node* temp = head;
+		if(temp->X==x && temp->Y==y) {
+			DelFirst();
+			return 0;
+		}
+		else {
+			node * prev = head;
+			temp = head->next;
+			while(temp!=NULL) {
+				if(temp->X==x && temp->Y==y) {
+					prev->next=temp->next;
+					temp->next=NULL;
+					delete temp;
+					return 0;
+				}
+				else {
+					prev = temp;
+					temp = temp->next;
+				}
 			}
 		}
 	}
+	return -1;
+}
+
+vector<int> points[2];
+int search(float d)
+{
+	if(head==NULL) return -1;
+	else {
+		node* temp = head;
+		while(temp!=NULL) {
+			float dist = sqrt((temp->X*temp->X) + (temp->Y*temp->Y));
+			if(dist<=d) {
+				points[0].push_back(temp->X);
+				points[1].push_back(temp->Y);
+				temp = temp->next;
+			}
+			else temp=temp->next;
+		}
+	}
+	if(points[0].size()==0) return -1;
+}
+
+loo search(loo x, loo y)
+{
+	if(head==NULL) return -1;
+	else {
+		node* temp = head;
+		while(temp!=NULL) {
+			if(temp->X==x && temp->Y==y) return 0;
+			else temp=temp->next;
+		}
+		return -1;
+	}
+	return -1;
+}
+
+loo length()
+{
+	loo count = 0;
+	if(head==NULL) return 0;
+	else {
+		node* temp = head;
+		while(temp!=NULL) {
+			count++;
+			temp = temp->next;
+		}
+	}
+	return count;
 }
 
 int main()
 {
-	int n; // no of queries
+	loo n; // no of queries
 	cin >> n;
-	int funcs[n];
-	vector<int> coords[n];
+	loo funcs[n];
+	loo coords[n][2];
 
 	// taking query inputs
-	for(int i=0; i<n; i++) {
+	for(loo i=0; i<n; i++) {
 		cin >> funcs[i];
 		if(funcs[i]==1 || funcs[i]==3 || funcs[i]==5) {
-			int x,y;
+			loo x,y;
 			cin >> x >> y;
-			coords[i].push_back(x);
-			coords[i].push_back(y);
+			coords[i][0]=x;
+			coords[i][1]=y;
 		}
 		else if(funcs[i]==4) {
-			int d;
+			loo d;
 			cin >> d;
-			coords[i].push_back(d);
+			coords[i][0]=d;
 		}
 	}
 
-	node* head = NULL;
-	for(int i=0; i<n; i++) {
-		if(funcs[i]==1) AddFirst(head, coords[i][0], coords[i][1]);
+	for(loo i=0; i<n; i++) {
+		if(funcs[i]==1) AddFirst(coords[i][0], coords[i][1]);
+		else if(funcs[i]==2) {
+			int a = DelFirst();
+			cout << a << endl;
+		}
+		else if(funcs[i]==3) {
+			int a = Del(coords[i][0],coords[i][1]);
+			cout << a << endl;
+		}
+		else if(funcs[i]==4) {
+			int a = search(coords[i][0]);
+			if(a==-1) cout << a << endl;
+			else {
+				while(!points[0].empty()) {
+					cout << "(" << points[0].back() << "," << points[1].back() << ")" << " ";
+					points[0].pop_back();
+					points[1].pop_back();
+				}
+				cout << endl;
+			}
+		}
+		else if(funcs[i]==5) {
+			int a = search(coords[i][0],coords[i][1]);
+			if(a==0) cout << "True" << endl;
+			else cout << "False" << endl;
+		}
+		else if(funcs[i]==6) {
+			cout << length() << endl;
+		}
 	}
-
-	cout << head->X << endl;
 
 	return 0;
 }
