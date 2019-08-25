5ddd86f2475fde4d0c2795a1e1fd219af7439f7e
bharasagarn <bharasagarn@gmail.com>
2019-08-26 01:27:18 +0530

    modified to accommodate unary negation input

--- Lab_4/Lab4_Assignment.cpp
+++ Lab_4/Lab4_Assignment.cpp
@@ -5,7 +5,7 @@ typedef long long int loo;
 #define MAX 1000
 #define EMPTY -1
 
-loo num(char c) 
+loo num(char c)
 {
 	return ( (loo)(c) - (loo)'0' );
 }
@@ -19,32 +19,41 @@ string toString(loo a)
 	return s;
 }
 
-string cToString(char x) 
-{ 
-    string s(1, x); 
-    return s;    
+string cToString(char x)
+{
+    string s(1, x);
+    return s;
 }
 
-loo prec(char c) 
-{ 
-    if(c == '^') 
-    return 3; 
-    else if(c == '*' || c == '/') 
-    return 2; 
-    else if(c == '+' || c == '-') 
-    return 1; 
+loo toInt(string s)
+{
+    stringstream ss;
+    ss << s;
+    loo a;
+    ss >> a;
+    return a;
+}
+
+loo prec(char c)
+{
+    if(c == '^')
+    return 3;
+    else if(c == '*' || c == '/')
+    return 2;
+    else if(c == '+' || c == '-')
+    return 1;
     else
-    return -1; 
+    return -1;
 }
 
 void toPostfix(string s, vector<string>& post)
 {
-	stack<char> st; 
-    st.push('N'); 
-    loo l = s.length(); 
-    loo i = 0; 
-    while(i<s.size()) 
-    { 
+	stack<char> st;
+    st.push('N');
+    loo l = s.length();
+    loo i = 0;
+    while(i<s.size())
+    {
         if(s[i]>='0' && s[i]<='9') {
         	loo p = 0;
         	p = (num(s[i]));
@@ -54,44 +63,58 @@ void toPostfix(string s, vector<string>& post)
         		i++;
         	}
         	post.push_back(toString(p));
-        } 
-        else if(s[i] == '(') {
-        	st.push('(');
-        	i++;
         }
-        else if(s[i] == ')') 
-        { 
-            while(st.top() != 'N' && st.top() != '(') 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-                post.push_back(cToString(c)); 
-            } 
-            if(st.top() == '(') 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-            } 
+        else if(s[i] == '(') {
+        	if(s[i+1]=='-') {
+        		i+=2;
+        		loo p = (num(s[i]));
+	        	i++;
+	        	while(s[i]>='0' && s[i]<='9' && i<s.size()) {
+	        		p = (p*10 + num(s[i]));
+	        		i++;
+	        	}
+	        	string res = "-" + toString(p);
+	        	post.push_back(res);
+	        	i++;
+        	}
+        	else {
+        		st.push('(');
+        		i++;
+        	}	
+        }
+        else if(s[i] == ')')
+        {
+            while(st.top() != 'N' && st.top() != '(')
+            {
+                char c = st.top();
+                st.pop();
+                post.push_back(cToString(c));
+            }
+            if(st.top() == '(')
+            {
+                char c = st.top();
+                st.pop();
+            }
             i++;
-        } 
-        else{ 
-            while(st.top() != 'N' && prec(s[i])<=prec(st.top())) 
-            { 
+        }
+        else{
+            while(st.top() != 'N' && prec(s[i])<=prec(st.top()))
+            {
             	if(s[i]=='^' && st.top()=='^') break;
-                char c = st.top(); 
-                st.pop(); 
-                post.push_back(cToString(c)); 
-            } 
+                char c = st.top();
+                st.pop();
+                post.push_back(cToString(c));
+            }
             st.push(s[i]);
-            i++; 
-        } 
-  
-    }  
-    while(st.top() != 'N') 
-    { 
-        char c = st.top(); 
+            i++;
+        }
+
+    }
+    while(st.top() != 'N')
+    {
+        char c = st.top();
         st.pop();
-        post.push_back(cToString(c)); 
+        post.push_back(cToString(c));
     }
 }
 
@@ -100,13 +123,13 @@ typedef struct tree {
 	tree* left, *right;
 } node;
 
-bool isOperator(string c) 
-{ 
-    if (c == "+" || c == "-" || 
-            c == "*" || c == "/" || 
-            c == "^") 
-        return true; 
-    return false; 
+bool isOperator(string c)
+{
+    if (c == "+" || c == "-" ||
+            c == "*" || c == "/" ||
+            c == "^")
+        return true;
+    return false;
 }
 
 node* newNode(string s)
@@ -156,7 +179,16 @@ loo calculate(loo a, loo b, string s)
 loo solve(node* t) {
 	if(t==NULL) return 0;
 	else {
-		if(!isOperator(t->value)) return (loo)stoi(t->value);
+		if(!isOperator(t->value)) {
+			string val = t->value;
+			if(val[0]=='-') {
+				string val1 = val.substr(1,val.size()-1);
+				return ((loo)toInt(val1))*(-1);
+			}
+			else {
+				return (loo)toInt(val);
+			}
+		} 
 		loo a = solve(t->left);
 		loo b = solve(t->right);
 		return calculate(a,b,t->value);
@@ -186,7 +218,7 @@ int main()
 		}
 	}
 
-	
+
 
 	return 0;
 }
