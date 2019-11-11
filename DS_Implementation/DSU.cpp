4b49dd3c252264ddf084b2164e5a4c12a1605ad1
bharasagarn <bharasagarn@gmail.com>
2019-11-11 15:48:55 +0530

    modified DSU.cpp

--- DS_Implementation/DSU.cpp
+++ DS_Implementation/DSU.cpp
@@ -21,14 +21,31 @@ typedef long long int loo;
 #define INF 1000000000
 #define M 1000000007
 
+struct Edge { 
+    int src, dest; 
+}; 
+
+struct Graph { 
+    loo V, E; 
+    struct Edge* edge; 
+};
+
 struct subset { 
     loo parent; 
     loo rank; 
 };
 
 loo find(struct subset subsets[], loo i) { 
-    if (subsets[i].parent != i) subsets[i].parent = find(subsets, subsets[i].parent);
+    if(subsets[i].parent != i) subsets[i].parent = find(subsets, subsets[i].parent);
     return subsets[i].parent; 
+}
+
+struct Graph* createGraph(loo V, loo E) { 
+    struct Graph* graph = (struct Graph*) malloc( sizeof(struct Graph) ); 
+    graph->V = V; 
+    graph->E = E; 
+    graph->edge = (struct Edge*) malloc( graph->E * sizeof( struct Edge ) ); 
+    return graph; 
 }
 
 void Union(struct subset subsets[], loo x, loo y) { 
@@ -46,8 +63,37 @@ void Union(struct subset subsets[], loo x, loo y) {
     } 
 }
 
+loo isCycle(struct Graph* graph) { 
+    loo V = graph->V; 
+    loo E = graph->E;  
+    struct subset *subsets = (struct subset*)malloc(V*sizeof(struct subset)); 
+  
+    loop(v,0,V) { 
+        subsets[v].parent = v; 
+        subsets[v].rank = 0; 
+    } 
+    loop(e,0,E) { 
+        loo x = find(subsets, graph->edge[e].src); 
+        loo y = find(subsets, graph->edge[e].dest); 
+        if (x == y) return 1;
+        Union(subsets, x, y); 
+    } 
+    return 0; 
+} 
+
+
 int main() {
-	
+	loo V,E;
+    cin >> V >> E;
+    struct Graph* g = createGraph(V,E);
+    loop(i,0,E) {
+        loo s,d;
+        cin >> s >> d;
+        g->edge[i].src = s;
+        g->edge[i].dest = d;
+    }
+    if(isCycle(g)) cout << "yes, cycle." << endl;
+    else cout << "no cycle." << endl;
 
 	return 0;
 }
